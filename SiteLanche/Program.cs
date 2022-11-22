5c292ef7cc0c3137bdc00193822ac2839f9dde4e
João Elias Maccari <joaomaccari@hotmail.com>
2022-11-22 14:32:06 -0300

    Projeto Inicial Venda Lanches

--- SiteLanche/Program.cs
+++ SiteLanche/Program.cs
@@ -1,27 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace SiteLanche;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        CreatHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreatHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
+   
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+  
