79a7e13ff31a5cfa5ed7d885e456cf335eaaf0f3
João Elias Maccari <joaomaccari@hotmail.com>
2022-11-24 15:00:06 -0300

    conexao db

---
+++ SiteLanche/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using SiteLanche.Models;
+
+namespace SiteLanche.Context {
+
+    public class AppDbContext : DbContext {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options) 
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }    
+
+
+        public DbSet<Lanche> Lanches { get; set; }  
+
+
+    }
+}
--- SiteLanche/Startup.cs
+++ SiteLanche/Startup.cs
@@ -1,4 +1,7 @@
-﻿namespace SiteLanche;
+﻿using Microsoft.EntityFrameworkCore;
+using SiteLanche.Context;
+
+namespace SiteLanche;
 
     public class Startup
     {
@@ -12,6 +15,10 @@
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
             services.AddControllersWithViews();
         }
 
